c06e0aa3f1ed7e5154b056a74f7bd7cad077c416
emanuel epifani <emanuel.epifani@overtechconsulting.com>
2025-10-23 12:00:37 +0200

    add some backlog things todo

--- backlog.ts
+++ backlog.ts
@@ -7,19 +7,26 @@
  */
 
 /** BUG found
- * - sometimes signUpUser() return 422 instead ofinsted 200 also if user does not exist
  *
  */
 
 /** DOCS
  * - add docs flow registration user, pet and device
- * - add schema excalidraw of archtecture
  *
  */
 
-/** TODAY
+/** QUESTION for team
  */
 
-/** DOMANDE x il team
- * - invece per le ACTIVITY li avrà senso testare ogni device KIPPY e PETLINK corretto?
+/** TECH-DEBT to fix
+ * - PerformanceTracker not atomic (depends by rm -rf test-reports on package.json)
+ * - globalSetup not in the sme process of test? not available all of my client (understand better vitest docs)
+ * - delete log file
+ * - centralize log level on env-schema-validation with default
+ * - exporte just 1 fixtures (not like now fixtures + fixturesCurrentBrand)
+ * - [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+ */
+
+/** TODO:
+ * - add assert on timestamp puchased sucription (how? now() + 1week/month/year +-how much?)
  */
