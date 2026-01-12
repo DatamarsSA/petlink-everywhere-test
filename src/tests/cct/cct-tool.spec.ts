2201fd8350e727fa61e582bb866a44ce368f568a
emanuel epifani <emanuel.epifani@overtechconsulting.com>
2026-01-12 18:13:09 +0100

    fix cleanup before cct tests

--- src/tests/cct/cct-tool.spec.ts
+++ src/tests/cct/cct-tool.spec.ts
@@ -165,6 +165,7 @@ describe("CCT Tool", () => {
     let setup: TestSetup;
 
     beforeAll(async () => {
+      await testHelper.cleanupAll();
       setup = await testHelper.setupBuilder().withUser().withDog().withDogDevice().build();
       await sentinelTcpSocketClient.connect();
     });
