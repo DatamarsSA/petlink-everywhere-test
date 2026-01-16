753bc221227962f6a6d97085ce76276e15209f93
emanuel epifani <emanuel.epifani@overtechconsulting.com>
2026-01-16 18:33:10 +0100

    [fix] add status ESZ on hb to sentinel

--- src/tests/user/mode/mode-energy-saving-zone.test.ts
+++ src/tests/user/mode/mode-energy-saving-zone.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, beforeAll, afterAll, expect } from "vitest";
 import { petlink } from "../../../clients/petlink-infrastructure/client-petlink-infrastructure.js";
 import { sentinelTcpSocketClient } from "../../../clients/sentinel/client-sentinel.js";
-import { Packet01, PacketType } from "../../../clients/sentinel/packets.js";
+import { OperatingStatus, Packet01, PacketType } from "../../../clients/sentinel/packets.js";
 import { testHelper, TestSetup } from "../../../clients/client-test-helper.js";
 import * as subscriptions from "../../../clients/petlink-infrastructure/endpoints/graphql/operations/core/subscriptions.js";
 import {
@@ -118,6 +118,7 @@ describe("Energy Saving Zone", () => {
     const eszEnterPayload = {
       latitude: 44.5024,
       longitude: 11.3463,
+      curr_status: OperatingStatus.DEFAULT,
       spare_c5: Packet01.D2SWelcomeHeartBeat.SpareC5.NDetached,
     };
 
@@ -148,6 +149,7 @@ describe("Energy Saving Zone", () => {
       latitude: 44.5024,
       longitude: 11.3463,
       spare_c5: 0x00,
+      curr_status: OperatingStatus.DEFAULT,
     };
 
     // Start listening for ESZ exit event with onReady callback
